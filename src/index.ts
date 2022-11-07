6144b4e724298db8ae196256f2c364849599b54a
Jhonny <gonzodzen@mail.ru>
2022-11-07 11:59:10 +0300

    Refactoring

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,33 @@ import express from 'express'
 const app = express()
 const port = 3000
 
+const db = {
+  courses: [
+  {id:1,title:'Front-end'},
+  {id:2,title:'Back-end'},
+  {id:3,title:'TS'},
+  {id:4,title:'Node JS'}]
+}
+
+
 app.get('/', (req, res) => {
   res.json({message: 'Hi, idiots...!'})
 })
-app.get('/users', (req, res) => {
-  res.send({messege: 'Hello User!.!.!'})
+
+app.get('/courses', (req, res) => {
+  const foundCourses = db.courses
+  .filter(c=> c.title.indexOf(req.query.title as string) > -1)
+    res.json(foundCourses)
 })
-app.post('/users', (req, res) => {
-  res.send('We created users!.!')
-})
-app.put('/users', (req, res) => {
-  res.send('What the fack!?!')
-})
-app.post('/users/5', (req, res) => {
-  res.send('What the fack!?! I`m users 5!')
+
+app.get('/courses/:id', (req, res) => {
+  const foundCourse = db.courses.find(c => c.id === +req.params.id)
+
+    if(!foundCourse){
+      res.sendStatus(404)
+      return
+    }
+    res.json(foundCourse)
 })
 
 
